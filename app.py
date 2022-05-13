dc4532b86553f9c138f1253c48f5603fd8950b3a
Ewen Bouquet <ewen.bouquet@gmail.com>
2022-05-13 14:16:50 +0200

    Remove trolololed hello world

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	x = 'world'
-	return f"Hello, {x}"
+	return "Hello, world"
 
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
